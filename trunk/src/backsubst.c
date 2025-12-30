ab59b1d4dd5888ec9e71090be8475bd5f71d0e5c
WicioB <boczekwiktor@gmail.com>
2025-12-30 11:50:14 +0100

    wyjątki

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -4,19 +4,23 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
-
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-				int pom;
-				int i;
-				for (i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+int  backsubst(Matrix *x, Matrix *mat, Matrix *b) 
+{
+	int i,j;
+	int pom = mat->r;
+	
+	if(pom != mat->c || pom != b->r || pom != x->r)
+	{
+		return 2; // zwracanie wartosci 2 jesli sa nieprawidlowe rozmiary
+	}
+	for(int i = pom-1 ; i>=0;i--)
+	{
+		if(mat->data[i][i] ==0)
+		{
+			return 1; //zwracanie 1 jesli ma miejsce dzielenie przez 0
+		}
+	}
+	return 0;
 }
 
 
